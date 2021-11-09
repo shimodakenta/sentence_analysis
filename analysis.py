be7f943da456ebd9272b1dc1fcbad18ff5a2d9b9
shimodakenta <68770039+shimodakenta@users.noreply.github.com>
2021-11-10 07:34:00 +0900

    CSV出力を実装

--- analysis.py
+++ analysis.py
@@ -1,23 +1,31 @@
 import collections
+import csv
 import pykakasi
 
-f = open('code.txt', 'r', encoding='UTF-8')
-data = f.read()
-f.close()
+# ファイル読み込み
+with open('freewriting.txt', 'r', encoding='UTF-8') as f:
+  txt = ''
+  for item in pykakasi.kakasi().convert(f.read()):
+    txt += item['hira']
 
-txt = ''
-for item in pykakasi.kakasi().convert(data):
-  txt += item['hira']
-
+# 単語長ごとに解析する
 for i in range(1, 5):
   print('----------------')
   print(f'{i}_gram')
+
+  # 頻度解析
   n_gram =[]
   for j in range(len(txt) - i):
     n_gram.append(txt[j : j + i])
   c = collections.Counter(n_gram)
   print(c.most_common(100))
 
+  # CSV出力
+  with open(f'{i}_gram.csv', 'w') as f:
+    for row in c.most_common():
+        print(*row, sep=',', file=f)
+
+  # グラフ出力
   # d = list(data)
   # sns.set(context="talk")
   # fig = plt.subplots(figsize=(8, 8))
